b59823e3c18b0ad72347029cc548e1cb46fa1d8b
Ayoub Darkaoui <ayoub.darka@gmail.com>
2025-10-12 18:12:31 +0200

    Exo1 TD1

--- TD1/Exercice1/fibo.c
+++ TD1/Exercice1/fibo.c
@@ -1,1 +1,23 @@
-#include <stdio.h>
+#include "fibo.h"
+
+long long fibo(int n){
+    if(n<=1){
+        return n;
+    }
+    return fibo(n-2)+fibo(n-1);
+}
+
+
+long long fibo_dynamic(int n,long long * tableFib){
+    if (n<=1){
+        return n;
+    }
+
+    if (tableFib[n]!=-1){
+        return tableFib[n];
+    }
+    else{
+        tableFib[n]=fibo_dynamic(n-1,tableFib)+fibo_dynamic(n-2,tableFib);
+        return tableFib[n];
+    }
+}
---
+++ TD1/Exercice1/fibo.h
@@ -0,0 +1,8 @@
+#ifndef FIBO_H
+#define FIBO_H
+
+long long fibo(int n);
+long long fibo_dynamic(int n, long long* tableFib);
+
+
+#endif 
