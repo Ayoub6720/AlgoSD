2f608599e38de8c38d4848388852bd2fe549b13a
Ayoub Darkaoui <ayoub.darka@gmail.com>
2025-10-22 21:18:45 +0200

    Exercice 2 fini

--- TD2/Exercice2/main.c
+++ TD2/Exercice2/main.c
@@ -6,22 +6,97 @@
 
 int main(void)
 {
-    // implement main here
-    // create array example using malloc
-    // use sorting algorithms and compare runtime
-    int n=50;
-    int *arr=malloc(n*sizeof(int));
+    FILE *f_in = fopen("data.txt", "r");
     
-    for (int i = 0; i < n; i++) {
-        arr[i] = rand() % 100;
-        printf("%d ", arr[i]);}
+
+    int n = 100000; 
+    int *arr = malloc(n * sizeof(int));
+    int *arr2 = malloc(n * sizeof(int));
+    int *arr3 = malloc(n * sizeof(int));
+    int *arr4 = malloc(n * sizeof(int));
+    int *arr5 = malloc(n * sizeof(int));
+
+    // Lecture des nombres
+    int count = 0;
+    while (count < n && fscanf(f_in, "%d", &arr[count]) == 1) {
+        count++;
+    }
+    fclose(f_in);
+
+    copy_array(arr,arr2,n);
+    copy_array(arr,arr3,n);
+    copy_array(arr,arr4,n);
+    copy_array(arr,arr5,n);
+
+
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+    quick_sort(arr, count);
+    end = clock();
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("Temps de quick sort : %.6f secondes\n", cpu_time_used);
+
+    start = clock();
+    merge_sort(arr2, count);
+    end = clock();
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("Temps de merge sort : %.6f secondes\n", cpu_time_used);
+
+    start = clock();
+    bubble_sort(arr3, count);
+    end = clock();
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("Temps de bubble sort : %.6f secondes\n", cpu_time_used);
     
-        
-    quick_sort(arr, n);
-    
-    
+    start = clock();
+    insertion_sort(arr4, count);
+    end = clock();
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("Temps de insertition sort : %.6f secondes\n", cpu_time_used);
+
+    start = clock();
+    selection_sort(arr5, count);
+    end = clock();
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("Temps de selection sort : %.6f secondes\n", cpu_time_used);
+
+    // Écriture dans data_out.txt
+    FILE *f_out = fopen("data_out.txt", "w");
+
+    for (int i = 0; i < count; i++) {
+        fprintf(f_out, "%d ", arr[i]);
+    }
     printf("\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);}
+    for (int i = 0; i < count; i++) {
+        fprintf(f_out, "%d ", arr2[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < count; i++) {
+        fprintf(f_out, "%d ", arr3[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < count; i++) {
+        fprintf(f_out, "%d ", arr4[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < count; i++) {
+        fprintf(f_out, "%d ", arr5[i]);
+    }
+
+    fclose(f_out);
+    free(arr);
+
     return 0;
 }
